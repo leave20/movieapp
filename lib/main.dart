99da70c5af12b89e308bab87327ce3a124051f05
leave20 <thetai6102@gmail.com>
2021-06-03 18:06:39 -0500

    init proyect

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:movieapp/src/pages/home_page.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App Movies',
+      initialRoute: '/',
+      routes: {
+        '/':(BuildContext context)=>HomePage(),
+      },
+    );
+  }
+}
